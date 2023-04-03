b83b53b0a43654a5813bbe90615ae32f81cce03f
Ahmed Muyen Kamran <akamran@colgate.edu>
2023-04-03 06:35:05 +0000

    to do list app with create, read, delete

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function getTodosFromLocalStorage() {
+  const todos = localStorage.getItem('todos');
+  if (todos) {
+    return JSON.parse(todos);
+  }
+  return [];
+}
 
 function App() {
+  const [todos, setTodos] = useState(getTodosFromLocalStorage())
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    const newTodo = event.target.elements[0].value;
+    setTodos([...todos, newTodo]);
+    event.target.reset();
+  }
+
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div className='App'>
+      <h1>To-Do List</h1>
+      <form onSubmit={addTodo}>
+        <input type="text" />
+        <button>Add</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
